069d9dc7ed2401f913149291f8995839424aa06a
Josh <joshray639@gmail.com>
2025-12-09 00:14:01 +0000

    Project 7.06

---
+++ Projects07/Project7.06.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("%d, %d, %d, %d, %d, %d", sizeof(int), sizeof(short), sizeof(long), sizeof(float), sizeof(double), sizeof(long double));
+
+    return 0;
+}
